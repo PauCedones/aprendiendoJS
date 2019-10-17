aa2ad193e832a5f0783f2d7c2b9b7d74285b8c0a
PauCedones <cedones@gmail.com>
2019-10-17 09:42:51 -0300

    ejercicio de lista de claves dle demonio

--- clase22 1.js
+++ clase22 1.js
@@ -16,5 +16,31 @@ alert (hacker);
 3) Acordate que las Ñ son fuertemente discriminadas, asi que te van a romper el codigo
 4) a la variable no hace falta ponerle una informacion ya que eso va a ir en un prompt despues
 
+Si tuviesemos que traducir lo que pusimos seria lo siguiente
+"Preguntale al usuario cual es su clave, mientras su clave no sea "paulatarjeta" se seguira preguntando en loop hasta que acierte con la pass"
+
 */
+
+
+let lista =[
+    "puerta 1",
+    "puerta 2",
+    "puerta 3",
+    "puerta 4",
+    "puerta 5",
+]
+
+let verdadero = true
+
+let abrir;
+
+while(verdadero){
+    abrir=prompt("que puerta es");
+    for(let i=0; i<4; i++){
+        if(abrir==lista [i]){
+        verdadero=false;
+        alert("pasaste")
+    }
+}
+} 
 
