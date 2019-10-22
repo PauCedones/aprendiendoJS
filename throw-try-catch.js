4cec55ed4eac28af7be2fb554a0c6ef0f865e311
PauCedones <cedones@gmail.com>
2019-10-22 11:07:02 -0300

    aprendimos finlally

--- throw-try-catch.js
+++ throw-try-catch.js
@@ -6,11 +6,15 @@
 3.En caso de que el dia ingresado no existam tirar un error y agarrarlo e imprimirlo.
 */
 
-
+/*1- hacer el prompt haciendo la pregunta*/
 let dia = prompt("Que dia de la semana te gusta mas?");
 
+/*2- haces que todo se escriba en minuscula*/
 dia = dia.toLowerCase();
 
+
+/* 3-Haces un try/catch adentro del switch para asegurar que se ejecute bien el codigo, 
+mas alla de que este el default*/
 try{
 
 switch(dia){
@@ -37,5 +41,15 @@ switch(dia){
 }
 }
 catch(err){
-    console.log(err);
+    console.warn(err);
 }
+
+/*ejemplo de finally */
+
+throw 500;
+finally{
+    console.log("Proceso terminado en x segundos");
+}
+
+console.log("mensaje nuevo");
+
