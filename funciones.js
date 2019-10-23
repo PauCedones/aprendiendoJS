ba06b3f9bf0a3b7873ab5985d3d652b4f61e72da
PauCedones <cedones@gmail.com>
2019-10-23 10:55:46 -0300

    ejercicio 23-10 primera parte

--- funciones.js
+++ funciones.js
@@ -34,3 +34,48 @@ const cuadrado =()=>{
 };
 
 cuadrado();
+
+
+//3.
+ /***********************************************
+ *
+ * 1. Crear un array vacío
+ * 2. Hacer una funcion para llenar
+ *    el array, pidiendo por prompt.
+ * 3. Hacer una función que muestre en un alert
+ *    el último valor agregado al array.
+ * 4. Hacer una función para imprimir la lista
+ *    de valores del array en un console.log.
+ * 
+ ************************************************/
+/* fijate que lo ideal es poner const comida para que sea un array constante */
+ let comida = [];
+
+ const pedido =()=>{
+    let pedir=prompt("que vas a ordenar?");
+    comida.push(pedir);     
+
+    //comida.push(prompt("que vas a ordernar")); otra forma de hacerlo
+ }
+
+ const ultimopedido=()=>{
+    alert(comida[comida.length -1]);
+
+ }
+
+ const pedidocliente=()=>{
+    for(i=0; i<comida.length; i++) {
+    console.log(comida[i]);
+
+    //forma mas facil pero no en renglones: console.log(comida);
+        }
+    }
+
+
+ pedido();
+ pedido();
+ pedido();
+ pedido();
+ ultimopedido();
+ pedidocliente();
+
